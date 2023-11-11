1a7554ae35e01d10ef97788cd02f828146000a46
Vikas-841 <150503522+Vikas-841@users.noreply.github.com>
2023-11-11 16:14:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
+API_ID       = 25826048
+API_HASH     = "b486ee260537697fdfc56b2b61cbc049"
 BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+SESSION      = "BQGKEwAAVMNRYYzTCQLk4YbeYloUpEKoPUimnDgClcgRHn1skOvRRNTkLOOxIQP8qoWV08Bu7yW-kvYosoVMnn3ks9nRNdoAOW613VhbLdeoJuDJkQVtKx07CsrQF2rWqqhnUEya8BWnwfAPwJHfSLEchZbnWsOx9yienioUKruDh-btGUx1fzbCPTCTGMxyVq1M1FkDvXPLLqDbbpDm4OhlMsu6E88Lo16o6NWadGa-nOsrPduinowbEaBPBHv75dBoSeSzzgnEirCioWH5eUuEX_DO5VnpcAYqQM3p6hA2CoFP5yC1nNqFtQXHu9kKoCBtXKWS0cxIb013P657Rf9UAuXEBAAAAAF49Z7aAA"
+DATABASE_URI = "mongodb+srv://Bikash:Bikash@bikash.yl2nhcy.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1002126379861
+ADMIN        = 6324330202
+CHANNEL      = "@deathshot_updates"
